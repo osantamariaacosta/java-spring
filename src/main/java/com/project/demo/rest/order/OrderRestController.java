1b8cc3b3aaf9d17ffc3a3008b5a6cdbca51132ed
olmansansoefe <olman.personal@gmail.com>
2024-09-27 16:40:20 -0600

    global response handler and pagination

--- src/main/java/com/project/demo/rest/order/OrderRestController.java
+++ src/main/java/com/project/demo/rest/order/OrderRestController.java
@@ -5,8 +5,15 @@ import com.project.demo.logic.entity.order.OrderRepository;
 import com.project.demo.logic.entity.user.User;
 import com.project.demo.logic.entity.user.UserRepository;
 import com.project.demo.logic.exceptions.ErrorResponse;
+import com.project.demo.logic.http.GlobalResponseHandler;
+import com.project.demo.logic.http.HttpResponse;
+import com.project.demo.logic.http.Meta;
+import jakarta.servlet.http.HttpServletRequest;
 import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -25,28 +32,60 @@ public class OrderRestController {
     @Autowired
     private UserRepository userRepository;
 
-    // /orders/user/{userId}
-    // /orders/user/20
-    @GetMapping("/user/{userId}")
-    public ResponseEntity<List<Order>> getAllByUser (@PathVariable Long userId) {
+    @GetMapping("/user/{userId}/orders")
+    public ResponseEntity<?> getAllByUser(
+            @PathVariable Long userId,
+            @RequestParam(defaultValue = "0") int page, // Número de página, por defecto 0
+            @RequestParam(defaultValue = "10") int size, // Tamaño de página, por defecto 10
+            HttpServletRequest request) {
+
         Optional<User> foundUser = userRepository.findById(userId);
-        if(foundUser.isPresent()) {
-            List<Order> orders = orderRepository.getOrderByUserId(userId);
-            return  new ResponseEntity<>(orders, HttpStatus.OK);
+
+        if (foundUser.isPresent()) {
+            Pageable pageable = PageRequest.of(page, size);
+            Page<Order> ordersPage = orderRepository.getOrderByUserId(userId, pageable);
+
+            Meta meta = new Meta(request.getMethod(), request.getRequestURL().toString());
+            meta.setTotalPages(ordersPage.getTotalPages());
+            meta.setTotalElements(ordersPage.getTotalElements());
+            meta.setPageNumber(ordersPage.getNumber());
+            meta.setPageSize(ordersPage.getSize());
+
+            return new GlobalResponseHandler().handleResponse(
+                    "Orders retrieved successfully",
+                    ordersPage.getContent(), // El contenido de la página
+                    HttpStatus.OK,
+                    meta
+            );
         } else {
-            throw new UsernameNotFoundException("User id " + userId + " not found");
+            return new GlobalResponseHandler().handleResponse(
+                    "User not found",
+                    null,
+                    HttpStatus.NOT_FOUND,
+                    request
+            );
         }
     }
 
     @PostMapping("/user/{userId}")
-    public ResponseEntity<Order> addOrderToUser(@PathVariable Long userId, @RequestBody Order order) {
+    public ResponseEntity<?> addOrderToUser(@PathVariable Long userId, @RequestBody Order order, HttpServletRequest request) {
         Optional<User> foundUser = userRepository.findById(userId);
         if(foundUser.isPresent()) {
             order.setUser(foundUser.get());
             Order savedOrder = orderRepository.save(order);
-            return new ResponseEntity<>(savedOrder, HttpStatus.CREATED);
+            return new GlobalResponseHandler().handleResponse(
+                    "Order created successfully",
+                    savedOrder,
+                    HttpStatus.CREATED,
+                    request
+            );
         } else {
-            throw new UsernameNotFoundException("User id " + userId + " not found");
+            return new GlobalResponseHandler().handleResponse(
+                    "User not found",
+                    null,
+                    HttpStatus.NOT_FOUND,
+                    request
+            );
         }
     }
 
@@ -86,7 +125,7 @@ public class OrderRestController {
             Optional<User> user = userRepository.findById(foundOrder.get().getUser().getId());
             user.get().getOrders().remove(foundOrder.get());
             orderRepository.deleteById(orderId);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            return new ResponseEntity<>(new HttpResponse("Order deleted"), HttpStatus.OK);
         } else {
             ErrorResponse error =  new ErrorResponse("Order with ID " + orderId + " not found");
             return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
